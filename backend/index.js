40de1f4bed39cd485b14293b244caac4980e7637
sowmya-7648 <sowmya7648@gmail.com>
2025-07-06 22:05:27 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,44 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-dotenv.config({});
-const app = express();
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+
+dotenv.config();
+
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res)=>{
-    return res.json({
-        message: "I'm coming from backend",
-        success: true
-    })
-})
+const __dirname = path.resolve();
 
+//middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: 'https://localhost:5173',
+    origin: process.env.URL,
     credentials: true
 }
-
 app.use(cors(corsOptions));
 
+// yha pr apni api ayengi
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
 
-app.listen(PORT, ()=>{
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// })
+
+
+server.listen(PORT, () => {
     connectDB();
-    console.log(`Server listening at port ${PORT}`);
-})
+    console.log(`Server listen at port ${PORT}`);
+});
