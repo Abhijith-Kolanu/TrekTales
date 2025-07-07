d98437dfdbb717aa29fe14381c337c9184d4adf8
ShivaniParepally <shivaniparepally26@gmail.com>
2025-07-07 23:28:54 +0530

    updated backend dependencies and made login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from './components/Signup'
+import { Provider } from 'react-redux'
+import Login from './components/Login'
+import { Home } from 'lucide-react'
+import MainLayout from './components/MainLayout'
+import Profile from './components/Profile'
+// import store from './store'
+
+const browserRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/profile',
+        element: <Profile/>
+      }
+    ]
+  },
+  {
+    path:'/login',
+    element: <Login />
+  },
+  {
+    path:'/signup',
+    element: <Signup />
+  },
+])
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={browserRouter} />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+import { Provider } from 'react-redux'
+import { Toaster } from 'sonner'
+// import store from './store' // ✅ import your store
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      <App />
+      <Toaster/>
+    {/* </Provider> */}
   </StrictMode>,
 )
